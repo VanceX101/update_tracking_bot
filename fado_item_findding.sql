8ae118edae3a3181ea5a0da8953a446d801f6e48
Vance Nguyen <97419226+VanceX101@users.noreply.github.com>
2024-09-29 19:59:12 +0700

    fado_item_findding.sql

--- fado_item_findding.sql
+++ fado_item_findding.sql
@@ -2,17 +2,17 @@ SELECT
     order_items.order_item_id, 
     order_items.order_id, 
     order_items.asin,
-		order_items.item_status, 
-    orders.public_note, 
-    orders.private_note
+    order_items.item_status,
+    order_items.quantity AS fado_quantity,
+    order_items.tracking_number
 FROM
     order_items
 INNER JOIN
     orders
     ON order_items.order_id = orders.order_id
 WHERE
-    order_items.asin = '1684228417'
-		AND order_items.item_status = 5
-		AND order_items.language = 'us'
-    AND orders.private_note LIKE '%203-2182295-6837146%' 
-    AND FROM_UNIXTIME(orders.created_date, "%Y") >= 2024;
+    order_items.asin = '{{ $json["asin"] }}'
+    AND order_items.language = 'us'
+    AND orders.private_note LIKE '%{{ $json["orderID"] }}%'
+    AND FROM_UNIXTIME(orders.created_date, "%Y") >= 2024
+    AND order_items.item_status NOT IN (-4, 0);
